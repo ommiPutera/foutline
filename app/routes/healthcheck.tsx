389b281cb3836f426a885920fd3220e76cd1e31b
ommiputera <omiputerakarunia@gmail.com>
2023-10-27 14:01:12 +0700

    healthcheck

---
+++ app/routes/healthcheck.tsx
@@ -0,0 +1,22 @@
+import { type DataFunctionArgs } from '@remix-run/node'
+import { prisma } from '~/utils/prisma.server.ts'
+
+export async function loader({ request }: DataFunctionArgs) {
+  const host =
+    request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
+
+  try {
+    await Promise.all([
+      prisma.user.count(),
+      fetch(`${new URL(request.url).protocol}${host}`, { method: 'HEAD' }).then(
+        r => {
+          if (!r.ok) return Promise.reject(r)
+        },
+      ),
+    ])
+    return new Response('OK')
+  } catch (error: unknown) {
+    console.error(request.url, 'healthcheck ❌', { error })
+    return new Response('ERROR', { status: 500 })
+  }
+}
