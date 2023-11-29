c191f2fc320d933f18317f0754d603f2cc8eade2
ommiputera <omiputerakarunia@gmail.com>
2023-11-29 10:52:47 +0700

    update schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,30 +11,8 @@ async function seed() {
         role: 'OWNER',
         passwordHash:
           '$2a$10$Y632TQG4s/MdTmIBjHl3..YlKO5CtiFnOmRHP6McJyyh9DPPoz/9e',
-        projects: {
-          create: [
-            {
-              name: 'Naufal Ghifari',
-              description: 'Web Development / Personal Website',
-              type: 'Digital Product',
-              heroId: 'naufalHero',
-              liveLink: 'http://www.naufalghfr.com',
-              content: 'none',
-            },
-          ],
-        },
       },
-    }),
-    await prisma.user.create({
-      data: {
-        email: 'naufal@gmail.com',
-        username: 'Naufal',
-        fullName: 'Naufal Ghifari',
-        role: 'CLIENT',
-        passwordHash:
-          '$2a$10$MXqGNKtKwsjNJsSCSDLoHuI36q.NaK1TKOKm7HdBWB./CfUGlcBOe',
-      },
-    }),
+    })
   ]
   return Promise.all(users)
 }
