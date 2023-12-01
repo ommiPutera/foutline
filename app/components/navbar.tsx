cd47c58158376932761e235d670ec8e020419e82
ommiputera <omiputerakarunia@gmail.com>
2023-12-01 10:55:26 +0700

    add shadcn ui and test button component

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,16 +1,22 @@
 import { useRootLoader } from "~/utils/use-root-loader.tsx"
 import { getLogo } from "./omition-logo.tsx"
+import { Button } from "./ui/button.tsx"
+import { useNavigate } from "@remix-run/react"
 
 function Navbar() {
   const { user } = useRootLoader()
+  const navigate = useNavigate()
   const OmitionLogo = getLogo()
 
   if (user) return <></>
   return (
-    <div className="px-5vw max-w-7xl mx-auto h-20 flex items-center">
-      <nav className="flex justify-between w-full">
+    <div className="max-w-7xl mx-auto h-20 flex items-center">
+      <nav className="flex items-center justify-between w-full px-5vw">
         <OmitionLogo />
-        <div>right</div>
+        <div>
+          {/* <ButtonLink></ButtonLink> */}
+          <Button onClick={() => navigate('/login')}>Log In</Button>
+        </div>
       </nav>
     </div>
   )
--- app/utils/misc.tsx
+++ app/utils/misc.tsx
@@ -1,4 +1,6 @@
-import { useRouteError } from "@remix-run/react"
+import type { LinkProps } from "@remix-run/react";
+import { Link, useRouteError } from "@remix-run/react";
+import React from "react";
 
 function useCapturedRouteError() {
   const error = useRouteError()
@@ -11,7 +13,61 @@ function getErrorMessage(error: unknown) {
   return 'Unknown Error'
 }
 
+type AnchorProps = React.DetailedHTMLProps<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>
+const AnchorOrLink = React.forwardRef<
+  HTMLAnchorElement,
+  AnchorProps & {
+    reload?: boolean
+    to?: LinkProps['to']
+    prefetch?: LinkProps['prefetch']
+  }
+>(function AnchorOrLink(props, ref) {
+  const {
+    to,
+    href,
+    download,
+    reload = false,
+    prefetch,
+    children,
+    ...rest
+  } = props
+  let toUrl = ''
+  let shouldUserRegularAnchor = reload || download
+
+  if (!shouldUserRegularAnchor && typeof href === 'string') {
+    shouldUserRegularAnchor = href.includes(':') || href.startsWith('#')
+  }
+
+  if (!shouldUserRegularAnchor && typeof to === 'string') {
+    toUrl = to
+    shouldUserRegularAnchor = to.includes(':')
+  }
+
+  if (!shouldUserRegularAnchor && typeof to === 'object') {
+    toUrl = `${to.pathname ?? ''}${to.hash ? `#${to.hash}` : ''}${to.search ? `?${to.search}` : ''
+      }`
+    shouldUserRegularAnchor = to.pathname?.includes(':')
+  }
+
+  if (shouldUserRegularAnchor) {
+    return (
+      <a {...rest} download={download} href={href ?? toUrl} ref={ref}>
+        {children}
+      </a>
+    )
+  } else {
+    return (
+      <Link prefetch={prefetch} to={to ?? href ?? ''} {...rest} ref={ref}>
+        {children}
+      </Link>
+    )
+  }
+})
+
 export {
-  getErrorMessage,
+  AnchorOrLink, getErrorMessage,
   useCapturedRouteError
-}
+};
