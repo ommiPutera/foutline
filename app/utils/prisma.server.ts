b05cb9717f03b09ee63b6f204467df979cf81014
ommiputera <omiputerakarunia@gmail.com>
2023-12-13 00:44:05 +0700

    up to acc

--- app/utils/prisma.server.ts
+++ app/utils/prisma.server.ts
@@ -1,45 +1,47 @@
 import type {Session} from '@prisma/client'
-import {PrismaClient} from '@prisma/client'
-import {remember} from '@epic-web/remember'
-import chalk from 'chalk'
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+// import {remember} from '@epic-web/remember'
+// import chalk from 'chalk'
 import {redirect} from '@remix-run/node'
 
-const logThreshold = 500
+// const logThreshold = 500
 // const sessionExpirationTime = 1000 * 20
 const sessionExpirationTime = 1000 * 60 * 60 * 24 * 365
-const prisma = remember('prisma', getClient)
+// const prisma = remember('prisma', getClient)
+const prisma = new PrismaClient().$extends(withAccelerate())
 
-function getClient(): PrismaClient {
-  // NOTE: during development if you change anything in this function, remember
-  // that this only runs once per server restart and won't automatically be
-  // re-run per request like everything else is.
-  const client = new PrismaClient({
-    log: [
-      {level: 'query', emit: 'event'},
-      {level: 'error', emit: 'stdout'},
-      {level: 'info', emit: 'stdout'},
-      {level: 'warn', emit: 'stdout'},
-    ],
-  })
-  client.$on('query', async e => {
-    if (e.duration < logThreshold) return
-    const color =
-      e.duration < logThreshold * 1.1
-        ? 'green'
-        : e.duration < logThreshold * 1.2
-          ? 'blue'
-          : e.duration < logThreshold * 1.3
-            ? 'yellow'
-            : e.duration < logThreshold * 1.4
-              ? 'redBright'
-              : 'red'
-    const dur = chalk[color](`${e.duration}ms`)
-    console.info(`prisma:query - ${dur} - ${e.query}`)
-  })
-  // make the connection eagerly so the first request doesn't have to wait
-  void client.$connect()
-  return client
-}
+// function getClient(): PrismaClient {
+//   // NOTE: during development if you change anything in this function, remember
+//   // that this only runs once per server restart and won't automatically be
+//   // re-run per request like everything else is.
+//   const client = new PrismaClient({
+//     log: [
+//       {level: 'query', emit: 'event'},
+//       {level: 'error', emit: 'stdout'},
+//       {level: 'info', emit: 'stdout'},
+//       {level: 'warn', emit: 'stdout'},
+//     ],
+//   })
+//   client.$on('query', async e => {
+//     if (e.duration < logThreshold) return
+//     const color =
+//       e.duration < logThreshold * 1.1
+//         ? 'green'
+//         : e.duration < logThreshold * 1.2
+//           ? 'blue'
+//           : e.duration < logThreshold * 1.3
+//             ? 'yellow'
+//             : e.duration < logThreshold * 1.4
+//               ? 'redBright'
+//               : 'red'
+//     const dur = chalk[color](`${e.duration}ms`)
+//     console.info(`prisma:query - ${dur} - ${e.query}`)
+//   })
+//   // make the connection eagerly so the first request doesn't have to wait
+//   void client.$connect()
+//   return client
+// }
 
 async function createSession(
   sessionData: Omit<Session, 'id' | 'expirationDate' | 'createdAt'>,
