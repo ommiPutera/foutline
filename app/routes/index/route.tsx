70ed16d0b8507cda7199417046e25c5fbd7d5726
ommiputera <omiputerakarunia@gmail.com>
2024-02-12 13:49:35 +0700

    fix update

--- app/routes/index/route.tsx
+++ app/routes/index/route.tsx
@@ -1,44 +1,20 @@
 import Landing from '~/components/landing/index.tsx'
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from '~/components/ui/tabs.tsx'
-import Board from './board.tsx'
-import {json, type DataFunctionArgs} from '@remix-run/node'
-import {getUser} from '~/utils/session.server.ts'
-import type {Post} from '@prisma/client'
-import {useLoaderData} from '@remix-run/react'
-import {useRootLoader} from '~/utils/use-root-loader.tsx'
+import { type DataFunctionArgs, redirect } from '@remix-run/node'
+import { getUser } from '~/utils/session.server.ts'
+import type { Post } from '@prisma/client'
 
 export type LoaderData = {
   posts: Post[] | null
 }
 
-export async function loader({request}: DataFunctionArgs) {
+export async function loader({ request }: DataFunctionArgs) {
   const user = await getUser(request)
-  const posts: Post[] = await user?.posts
-
-  const data: LoaderData = {posts}
-  return json(data)
+  if (user) return redirect("/home")
+  return "/"
 }
 
 function Index() {
-  const {isAuthenticated} = useRootLoader()
-  const {posts} = useLoaderData<LoaderData>()
-
-  if (!isAuthenticated) return <Landing />
-  return (
-    <Tabs defaultValue="board">
-      <TabsList>
-        <TabsTrigger value="board">Semua</TabsTrigger>
-      </TabsList>
-      <TabsContent value="board" asChild>
-        <Board posts={posts as any} />
-      </TabsContent>
-    </Tabs>
-  )
+  return <Landing />
 }
 
 export default Index
