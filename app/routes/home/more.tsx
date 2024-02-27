48bc336d023b77a5b1522c5e63c3527958c17ee4
ommiputera <omiputerakarunia@gmail.com>
2024-02-27 15:23:09 +0700

    updte UI

--- app/routes/home/more.tsx
+++ app/routes/home/more.tsx
@@ -1,30 +1,30 @@
-import {ArrowUpRight, MoreHorizontal, Trash} from 'lucide-react'
+import { ArrowUpRight, MoreHorizontal, Trash } from 'lucide-react'
 
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from '~/components/ui/popover.tsx'
-import {SelectSeparator} from '~/components/ui/select.tsx'
-import {Button, ButtonLink} from '~/components/ui/button.tsx'
+import { SelectSeparator } from '~/components/ui/select.tsx'
+import { Button, ButtonLink } from '~/components/ui/button.tsx'
 
-import type {Post} from '@prisma/client'
+import type { Post } from '@prisma/client'
 
-import {useSubmit} from '@remix-run/react'
+import { useSubmit } from '@remix-run/react'
 
-import {getPostType} from '~/utils/get-post-type.ts'
+import { getPostType } from '~/utils/get-post-type.ts'
 
-import {FormType} from './route.tsx'
-import {useCardStore} from './card-item.tsx'
+import { FormType } from './route.tsx'
+import { useCardStore } from './card-item.tsx'
 
-function More({id, type}: Pick<Post, 'id' | 'type'>) {
-  const {setIdCardFocus} = useCardStore()
+function More({ id, type }: Pick<Post, 'id' | 'type'>) {
+  const { setIdCardFocus } = useCardStore()
   return (
     <Popover onOpenChange={v => (v ? setIdCardFocus(id) : setIdCardFocus(''))}>
       <div className="flex h-full">
         <PopoverTrigger asChild>
           <Button size="icon" variant="transparent" className="rounded-full">
-            <MoreHorizontal className="h-4 w-4 stroke-black dark:stroke-white" />
+            <MoreHorizontal className="h-4 w-4 stroke-foreground/80" strokeWidth={2.5} />
           </Button>
         </PopoverTrigger>
       </div>
@@ -42,7 +42,7 @@ function More({id, type}: Pick<Post, 'id' | 'type'>) {
   )
 }
 
-function Open({id, type}: Pick<Post, 'id' | 'type'>) {
+function Open({ id, type }: Pick<Post, 'id' | 'type'>) {
   return (
     <div className="my-2">
       <ButtonLink
@@ -59,14 +59,14 @@ function Open({id, type}: Pick<Post, 'id' | 'type'>) {
   )
 }
 
-function Remove({id}: Pick<Post, 'id'>) {
+function Remove({ id }: Pick<Post, 'id'>) {
   const submit = useSubmit()
   return (
     <div className="my-2">
       <Button
         variant="ghost"
         size="sm"
-        onClick={() => submit({id, _action: FormType.DELETE}, {method: 'POST'})}
+        onClick={() => submit({ id, _action: FormType.DELETE }, { method: 'POST' })}
         className="w-full justify-start rounded-none px-3"
       >
         <Trash size="16" className="mr-2" />
