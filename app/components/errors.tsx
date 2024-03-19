5c88db00bfa0d1ac67ade4037f202d609aa969e3
ommiputera <omiputerakarunia@gmail.com>
2024-03-19 13:42:30 +0700

    update hero

--- app/components/errors.tsx
+++ app/components/errors.tsx
@@ -17,19 +17,19 @@ function ErrorPage({
             padding: 30,
           }}
         >
-          <h1 style={{fontSize: '2em'}}>{title}</h1>
-          <p style={{fontSize: '1.5em'}}>{subtitle}</p>
+          <h1 style={{ fontSize: '2em' }}>{title}</h1>
+          <p style={{ fontSize: '1.5em' }}>{subtitle}</p>
           <small>
             Also, this site works much better with JavaScript enabled...
           </small>
         </div>
       </noscript>
       <main className="relative">
-        <h1 style={{fontSize: '2em'}}>{title}</h1>
-        <p style={{fontSize: '1.5em'}}>{subtitle}</p>
+        <h1 style={{ fontSize: '1.4em', marginBottom: '6px' }}>{title}</h1>
+        <p style={{ fontSize: '0.8em' }}>{subtitle}</p>
       </main>
     </>
   )
 }
 
-export {ErrorPage}
+export { ErrorPage }
--- app/components/landing/hero.tsx
+++ app/components/landing/hero.tsx
@@ -1,19 +1,23 @@
 function Hero() {
   return (
-    <div className="px-5vw mx-auto flex max-w-screen-xl gap-20 py-4 md:pb-44 md:pt-16">
-      <div className="w-full flex-1">
+    <div className="px-5vw mx-auto flex flex-col md:flex-row max-w-screen-xl gap-2 md:gap-8 py-4 md:pb-32">
+      <div className="w-full max-w-[450px] mt-24">
         <div className="flex flex-col gap-6">
-          <h2 className="max-w-xl text-4xl font-semibold leading-loose tracking-tighter md:text-5xl">
+          <h2 className="max-w-xl text-5xl font-normal tracking-tight md:max-w-lg md:text-5xl">
             Personal financial management and tracker.
           </h2>
-          <p className="text-base font-light text-black/70 dark:text-white/70 md:max-w-md md:font-light">
+          <p className="text-base font-light text-black/70 dark:text-white/70 md:max-w-xs md:font-light">
             Solusi manajamen keuangan yang berbasis{' '}
-            <b className="font-bold">Note-Taking app</b> yang memudahkan dalam
-            mempelajari dan merekem data keuangan Anda
+            <b className="font-bold">Note-Taking</b> yang memudahkan dalam
+            mempelajari dan merekem data keuangan Anda.
           </p>
         </div>
       </div>
-      <div className="w-full flex-1"></div>
+      <div className="w-full relative">
+        <div className="absolute left-0 top-0 w-[1000px] md:block hidden">
+          <img src="/hero.png" alt="" />
+        </div>
+      </div>
     </div>
   )
 }
