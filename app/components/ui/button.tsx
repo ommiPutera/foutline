33990380156143e4a272a41faa22f52147ef88d9
ommiputera <omiputerakarunia@gmail.com>
2024-03-19 21:20:31 +0700

    fix ui

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,18 +1,18 @@
-import {Slot} from '@radix-ui/react-slot'
-import {cva, type VariantProps} from 'class-variance-authority'
+import { Slot } from '@radix-ui/react-slot'
+import { cva, type VariantProps } from 'class-variance-authority'
 import * as React from 'react'
-import {cn} from '~/lib/utils.ts'
-import {AnchorOrLink} from '~/utils/misc.tsx'
+import { cn } from '~/lib/utils.ts'
+import { AnchorOrLink } from '~/utils/misc.tsx'
 
 const variants = {
   default:
     'bg-primary text-primary-foreground hover:bg-primary/90 focus-visible:bg-primary/90',
   destructive:
     'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-  outline: 'border border-input hover:bg-accent hover:text-accent-foreground',
+  outline: 'border border-input hover:bg-zinc-800 hover:text-accent-foreground',
   secondary:
     'bg-secondary dark:bg-zinc-800 text-secondary-foreground hover:bg-secondary/80',
-  ghost: 'hover:bg-secondary hover:dark:bg-zinc-800 focus-visible:bg-accent',
+  ghost: 'hover:bg-secondary hover:dark:bg-zinc-800 focus-visible:bg-zinc-800',
   transparent:
     'bg-none focus-visible:bg-none hover:[&:has(svg)]:before:bg-gray-100/10',
   link: 'text-primary underline-offset-4 hover:underline',
@@ -30,8 +30,8 @@ const buttonVariants = cva(
   'relative inline-flex items-center justify-center whitespace-nowrap font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-[1px] focus-visible:ring-ring focus-visible:ring-offset-0 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
-      variant: {...variants},
-      size: {...sizes},
+      variant: { ...variants },
+      size: { ...sizes },
     },
     defaultVariants: {
       variant: 'default',
@@ -42,18 +42,18 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(function Button(
-  {className, variant, size, asChild = false, ...props},
+  { className, variant, size, asChild = false, ...props },
   ref,
 ) {
   const Comp = asChild ? Slot : 'button'
   return (
     <Comp
-      className={cn(buttonVariants({variant, size, className}))}
+      className={cn(buttonVariants({ variant, size, className }))}
       ref={ref}
       {...props}
     />
@@ -67,8 +67,8 @@ Button.displayName = 'Button'
 const ButtonLink = React.forwardRef<
   HTMLAnchorElement,
   React.ComponentPropsWithRef<typeof AnchorOrLink> & ButtonProps
->(function ButtonLink({children, asChild = false, ...props}, ref) {
-  const {variant, size, disabled} = props
+>(function ButtonLink({ children, asChild = false, ...props }, ref) {
+  const { variant, size, disabled } = props
   return (
     <Button asChild variant={variant} size={size} className={props.className}>
       <AnchorOrLink ref={ref} disabled={disabled} {...props}>
@@ -79,4 +79,4 @@ const ButtonLink = React.forwardRef<
 })
 ButtonLink.displayName = 'ButtonLink'
 
-export {Button, ButtonLink, buttonVariants}
+export { Button, ButtonLink, buttonVariants }
